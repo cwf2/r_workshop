9c1a1603dd155726dd10992949a4fc4af3c3928e
Chris Forstall <forstall@buffalo.edu>
2014-04-11 22:46:02 -0400

    tweak brontes.R

--- brontes.R
+++ brontes.R
@@ -1,7 +1,8 @@
-
-corpus.dir <- 'brontes'
+# load pre-defined functions
+source('common.R')
 
 # load metadata
+corpus.dir <- 'brontes'
 
 file.metadata <- file.path(corpus.dir, 'metadata.txt')
 metadata <- read.table(file.metadata, sep='\t', header=TRUE, colClasses=c('character', 'factor', 'character', 'factor'))
@@ -25,6 +26,15 @@ tab.fingerprint <- t(sapply(doc.word.freq, Fingerprint, tokens=names(fingerprint
 pca.fingerprint <- prcomp(tab.fingerprint)
 
 # author/genre plot
-plot(pca.fingerprint$x, col=unclass(metadata$type), pch=unclass(metadata$author))
-legend('topright', legend=c('anne prose', 'charlotte prose', 'emily prose', 'anne verse', 'charlotte verse', 'emily verse'), pch=c(1:3,1:3), col=c(1,1,1,2,2,2), xpd=TRUE, cex=.8)
+plot(pca.fingerprint$x, col=unclass(metadata$type), pch=as.character(metadata$author))
+# legend('topright', legend=c('anne prose', 'charlotte prose', 'emily prose', 'anne verse', 'charlotte verse', 'emily verse'), pch=c(1:3,1:3), col=c(1,1,1,2,2,2), xpd=TRUE, cex=.8, inset=c(-.2, 0))
+title(main='Author and Genre\nAmong the Brontë Sisters')
 
+# identify points
+text(pca.fingerprint$x, labels=metadata$title, xpd=TRUE, cex=.6, pos=1)
+
+# separate by author
+# abline(h=-1.5)
+
+# separate by genre
+# abline(v=-7)
